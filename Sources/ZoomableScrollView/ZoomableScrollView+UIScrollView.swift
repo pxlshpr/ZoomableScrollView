2bf9334dce2b78ba98e52b0f3c105a1a09412b10
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-04 17:53:31 +0500

    tag increment

--- Sources/ZoomableScrollView/ZoomableScrollView+UIScrollView.swift
+++ Sources/ZoomableScrollView/ZoomableScrollView+UIScrollView.swift
@@ -2,53 +2,12 @@ import SwiftUI
 import VisionSugar
 import SwiftUISugar
 
-public class ZoomableUIScrollView: UIView {
-    
-    let delegate: UIScrollViewDelegate
-    let hostedView: UIView
-    
-    required init(delegate: UIScrollViewDelegate, hostedView: UIView) {
-        self.delegate = delegate
-        self.hostedView = hostedView
-        super.init(frame: CGRect.zero)
-        setupView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupView() {
-        hostedView.frame = scrollView.bounds
-        scrollView.addSubview(hostedView)
-        scrollView.setZoomScale(1, animated: true)
-        addSubview(scrollView)
-    }
-    
-    lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.delegate = delegate
-        scrollView.maximumZoomScale = 20
-        scrollView.minimumZoomScale = 1
-        scrollView.bouncesZoom = true
-        return scrollView
-    }()
-}
 extension ZoomableScrollView {
 
-//    func scrollView(context: Context) -> ZoomableUIScrollView {
-//        ZoomableUIScrollView(
-//            delegate: context.coordinator,
-//            hostedView: hostedView(context: context)
-//        )
-//    }
-    
     func hostedView(context: Context) -> UIView {
         let hostedView = context.coordinator.hostingController.view!
         hostedView.translatesAutoresizingMaskIntoConstraints = true
         hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        hostedView.frame = scrollView.bounds
-//        hostedView.backgroundColor = UIColor.systemGroupedBackground
         return hostedView
     }
     
@@ -60,18 +19,71 @@ extension ZoomableScrollView {
         scrollView.minimumZoomScale = 1
         scrollView.bouncesZoom = true
 
-        // create a UIHostingController to hold our SwiftUI content
-//        let hostedView = context.coordinator.hostingController.view!
-//        hostedView.translatesAutoresizingMaskIntoConstraints = true
-//        hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        hostedView.frame = scrollView.bounds
-//        hostedView.backgroundColor = UIColor.systemGroupedBackground
         let hosted = hostedView(context: context)
         hosted.frame = scrollView.bounds
         scrollView.addSubview(hosted)
 
         scrollView.setZoomScale(1, animated: true)
+        
+        scrollView.addTapGestureRecognizer { sender in
+            
+            let hostedView = hostedView(context: context)
+            let point = sender.location(in: hostedView)
+            let sizeToBaseRectOn = scrollView.frame.size
+//            let sizeToBaseRectOn = hostedView.frame.size
+            
+            let size = CGSize(width: sizeToBaseRectOn.width / 2,
+                              height: sizeToBaseRectOn.height / 2)
+            let zoomSize = CGSize(width: size.width / scrollView.zoomScale,
+                                  height: size.height / scrollView.zoomScale)
+
+            print("""
+Got a tap at: \(point), when:
+    hostedView.size: \(hostedView.frame.size)
+    scrollView.size: \(scrollView.frame.size)
+    scrollView.contentSize: \(scrollView.contentSize)
+    scrollView.zoomScale: \(scrollView.zoomScale)
+    size: \(size)
+    🔍 zoomSize: \(zoomSize)
+""")
+
+            let origin = CGPoint(x: point.x - zoomSize.width / 2,
+                                 y: point.y - zoomSize.height / 2)
+            scrollView.zoom(to:CGRect(origin: origin, size: zoomSize), animated: true)
+        }
 
         return scrollView
     }
+    
+    func zoomRectForScale(scale: CGFloat, center: CGPoint, scrollView: UIScrollView, context: Context) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = hostedView(context: context).frame.size.height / scale
+        zoomRect.size.width  = hostedView(context: context).frame.size.width  / scale
+        let newCenter = scrollView.convert(center, from: hostedView(context: context))
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
+    }
+        //    func userDoubleTappedScrollview(recognizer:  UITapGestureRecognizer) {
+        //        if (zoomScale > minimumZoomScale) {
+        //            setZoomScale(minimumZoomScale, animated: true)
+        //        }
+        //        else {
+        //            //(I divide by 3.0 since I don't wan't to zoom to the max upon the double tap)
+        //            let zoomRect = zoomRectForScale(scale: maximumZoomScale / 3.0, center: recognizer.location(in: recognizer.view))
+        //            zoom(to: zoomRect, animated: true)
+        //        }
+        //    }
+        //
+        //    func zoomRectForScale(scale : CGFloat, center : CGPoint) -> CGRect {
+        //        var zoomRect = CGRect.zero
+        //        if let imageV = self.viewForZooming {
+        //            zoomRect.size.height = imageV.frame.size.height / scale;
+        //            zoomRect.size.width  = imageV.frame.size.width  / scale;
+        //            let newCenter = imageV.convert(center, from: self)
+        //            zoomRect.origin.x = newCenter.x - ((zoomRect.size.width / 2.0));
+        //            zoomRect.origin.y = newCenter.y - ((zoomRect.size.height / 2.0));
+        //        }
+        //        return zoomRect;
+        //    }
 }
--- Sources/ZoomableScrollView/ZoomableScrollView.swift
+++ Sources/ZoomableScrollView/ZoomableScrollView.swift
@@ -38,29 +38,6 @@ public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
         scrollView(context: context)
     }
     
-//    func userDoubleTappedScrollview(recognizer:  UITapGestureRecognizer) {
-//        if (zoomScale > minimumZoomScale) {
-//            setZoomScale(minimumZoomScale, animated: true)
-//        }
-//        else {
-//            //(I divide by 3.0 since I don't wan't to zoom to the max upon the double tap)
-//            let zoomRect = zoomRectForScale(scale: maximumZoomScale / 3.0, center: recognizer.location(in: recognizer.view))
-//            zoom(to: zoomRect, animated: true)
-//        }
-//    }
-//
-//    func zoomRectForScale(scale : CGFloat, center : CGPoint) -> CGRect {
-//        var zoomRect = CGRect.zero
-//        if let imageV = self.viewForZooming {
-//            zoomRect.size.height = imageV.frame.size.height / scale;
-//            zoomRect.size.width  = imageV.frame.size.width  / scale;
-//            let newCenter = imageV.convert(center, from: self)
-//            zoomRect.origin.x = newCenter.x - ((zoomRect.size.width / 2.0));
-//            zoomRect.origin.y = newCenter.y - ((zoomRect.size.height / 2.0));
-//        }
-//        return zoomRect;
-//    }
-    
     public func makeCoordinator() -> Coordinator {
         return Coordinator(hostingController: UIHostingController(rootView: self.content))
     }
@@ -93,6 +70,59 @@ public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
         public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
             return hostingController.view
         }
+        
+        @objc func doubleTapped(recognizer:  UITapGestureRecognizer) {
+            
+        }
     }
 }
 
+
+import UIKit
+
+extension UIView {
+    
+    // In order to create computed properties for extensions, we need a key to
+    // store and access the stored property
+    fileprivate struct AssociatedObjectKeys {
+        static var tapGestureRecognizer = "MediaViewerAssociatedObjectKey_mediaViewer"
+    }
+    
+    fileprivate typealias Action = ((UITapGestureRecognizer) -> Void)?
+    
+    // Set our computed property type to a closure
+    fileprivate var tapGestureRecognizerAction: Action? {
+        set {
+            if let newValue = newValue {
+                // Computed properties get stored as associated objects
+                objc_setAssociatedObject(self, &AssociatedObjectKeys.tapGestureRecognizer, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            }
+        }
+        get {
+            let tapGestureRecognizerActionInstance = objc_getAssociatedObject(self, &AssociatedObjectKeys.tapGestureRecognizer) as? Action
+            return tapGestureRecognizerActionInstance
+        }
+    }
+    
+    // This is the meat of the sauce, here we create the tap gesture recognizer and
+    // store the closure the user passed to us in the associated object we declared above
+    public func addTapGestureRecognizer(action: ((UITapGestureRecognizer) -> Void)?) {
+        self.isUserInteractionEnabled = true
+        self.tapGestureRecognizerAction = action
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        tapGestureRecognizer.numberOfTapsRequired = 2
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // Every time the user taps on the UIImageView, this function gets called,
+    // which triggers the closure we stored
+    @objc fileprivate func handleTapGesture(sender: UITapGestureRecognizer) {
+        if let action = self.tapGestureRecognizerAction {
+            let location = sender.location(in: self)
+            action?(sender)
+        } else {
+            print("no action")
+        }
+    }
+    
+}
