a288d2f65306424dbc311556f59dee6a25e33402
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-20 09:09:27 +0500

    tag increment

--- Sources/ZoomableScrollView/ZoomableScrollView.swift
+++ Sources/ZoomableScrollView/ZoomableScrollView.swift
@@ -2,25 +2,6 @@ import SwiftUI
 import VisionSugar
 import SwiftUISugar
 
-/// This identifies an area of the ZoomableScrollView to focus on
-public struct FocusedBox {
-    
-    /// This is the boundingBox (in terms of a 0 to 1 ratio on each dimension of what the CGRect is (similar to the boundingBox in Vision)
-    let boundingBox: CGRect
-    let padded: Bool
-    let animated: Bool
-    let imageSize: CGSize
-    
-    public init(boundingBox: CGRect, animated: Bool = true, padded: Bool = true, imageSize: CGSize) {
-        self.boundingBox = boundingBox
-        self.padded = padded
-        self.animated = animated
-        self.imageSize = imageSize
-    }
-    
-    public static let none = Self.init(boundingBox: .zero, imageSize: .zero)
-}
-
 public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
     
     @State var lastFocusedArea: FocusedBox? = nil
@@ -29,18 +10,18 @@ public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
     let backgroundColor: UIColor?
     private var content: Content
     
-    var focusedBox: Binding<FocusedBox?>?
+//    var focusedBox: Binding<FocusedBox?>?
     var zoomBox: Binding<FocusedBox?>?
 
     public init(
-        focusedBox: Binding<FocusedBox?>? = nil,
+//        focusedBox: Binding<FocusedBox?>? = nil,
         zoomBox: Binding<FocusedBox?>? = nil,
         backgroundColor: UIColor? = nil,
         @ViewBuilder content: () -> Content
     ) {
         self.backgroundColor = backgroundColor
         self.content = content()
-        self.focusedBox = focusedBox
+//        self.focusedBox = focusedBox
         self.zoomBox = zoomBox
     }
     
@@ -50,6 +31,7 @@ public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
             await MainActor.run { scrollView.setZoomScale(1.01, animated: false) }
             await MainActor.run { scrollView.setZoomScale(1, animated: false) }
         }
+        
         return scrollView
     }
     
@@ -92,30 +74,60 @@ public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
     // MARK: - Coordinator
     public class Coordinator: NSObject, UIScrollViewDelegate {
         var hostingController: UIHostingController<Content>
+        var scrollView: UIScrollView? = nil
         
         init(hostingController: UIHostingController<Content>) {
             self.hostingController = hostingController
+            super.init()
+            NotificationCenter.default.addObserver(self, selector: #selector(focusZoomableScrollView), name: .focusZoomableScrollView, object: nil)
+        }
+        
+        @objc func focusZoomableScrollView(notification: Notification) {
+            guard let focusedBox = notification.userInfo?[Notification.ZoomableScrollViewKeys.focusedBox] as? FocusedBox,
+                  let scrollView
+            else {
+                return
+            }
+            
+            if focusedBox.boundingBox == .zero, scrollView.zoomScale != 1 {
+                scrollView.setZoomScale(1, animated: false)
+            } else {
+                scrollView.focus(on: focusedBox)
+            }
         }
         
         public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+            self.scrollView = scrollView
             return hostingController.view
-        }
-        
-        @objc func doubleTapped(recognizer:  UITapGestureRecognizer) {
-            
-        }
-        
-        public func scrollViewDidZoom(_ scrollView: UIScrollView) {
-            print("🔍 zoomScale is \(scrollView.zoomScale)")
         }
     }
 }
 
+extension Notification.Name {
+    public static var focusZoomableScrollView: Notification.Name { return .init("focusZoomableScrollView") }
+}
 
-func sleepTask(_ seconds: CGFloat, tolerance: Int = 1) async throws {
-    try await Task.sleep(
-        until: .now + .seconds(seconds),
-        tolerance: .seconds(tolerance),
-        clock: .suspending
-    )
+extension Notification {
+    public struct ZoomableScrollViewKeys {
+        public static let focusedBox = "focusedBox"
+    }
+}
+
+/// This identifies an area of the ZoomableScrollView to focus on
+public struct FocusedBox {
+    
+    /// This is the boundingBox (in terms of a 0 to 1 ratio on each dimension of what the CGRect is (similar to the boundingBox in Vision)
+    let boundingBox: CGRect
+    let padded: Bool
+    let animated: Bool
+    let imageSize: CGSize
+    
+    public init(boundingBox: CGRect, animated: Bool = true, padded: Bool = true, imageSize: CGSize) {
+        self.boundingBox = boundingBox
+        self.padded = padded
+        self.animated = animated
+        self.imageSize = imageSize
+    }
+    
+    public static let none = Self.init(boundingBox: .zero, imageSize: .zero)
 }
